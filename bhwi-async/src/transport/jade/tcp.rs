f2ac0e72be1be29efcbb2fce4704ce896666896d
Trevor Arjeski <tmarjeski@gmail.com>
2026-04-10 18:33:16 +0000

    bhwi-cli(jade): fix serial transport reading
    - Fix bug where reading from the serial stream was hanging since it wasn't using
    cbor framed streaming. Didn't catch this cause I only checked with WASM and the
    emulator.
    - Refactored and renamed TcpClient trait to CborStream and implemented it for
    SerialTransport
    - Change PinServerClient HttpClient request header to be json for correctness

--- bhwi-async/src/transport/jade/tcp.rs
+++ bhwi-async/src/transport/jade/tcp.rs
@@ -1,7 +1,6 @@
 use async_trait::async_trait;
-use serde_cbor::Value;
 
-use crate::Transport;
+use crate::{Transport, transport::jade::CborStream};
 
 pub struct TcpTransport<C> {
     pub client: C,
@@ -14,38 +13,11 @@ impl<C> TcpTransport<C> {
 }
 
 #[async_trait(?Send)]
-pub trait TcpClient {
-    async fn write_all(&mut self, command: &[u8]) -> Result<(), std::io::Error>;
-    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error>;
-}
-
-#[async_trait(?Send)]
-impl<C: TcpClient> Transport for TcpTransport<C> {
+impl<C: CborStream> Transport for TcpTransport<C> {
     type Error = std::io::Error;
 
     async fn exchange(&mut self, command: &[u8], _encrypted: bool) -> Result<Vec<u8>, Self::Error> {
         self.client.write_all(command).await?;
-
-        let mut buf = Vec::new();
-        let mut temp = [0u8; 1024];
-
-        // HACK: i don't know a better way right now!
-        loop {
-            let n = self.client.read(&mut temp).await?;
-            // XXX: what happens when n is 0?
-            buf.extend_from_slice(&temp[..n]);
-            let mut cursor = std::io::Cursor::new(&buf);
-            match serde_cbor::from_reader::<Value, _>(&mut cursor) {
-                Ok(_) => {
-                    return Ok(buf);
-                }
-                Err(e) if e.is_io() => {
-                    continue; // read more bytes
-                }
-                Err(e) => {
-                    return Err(std::io::Error::other(e));
-                }
-            }
-        }
+        self.client.read_cbor_message().await
     }
 }
